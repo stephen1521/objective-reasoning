6a08ca1edb906a3bfa7d5df8251f2c1264e30ac3
stephen1521 <stephen.warnock@codeimmersives.com>
2022-10-11 12:28:05 -0400

    updating files

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(user){
+    return user.userRole === 'ADMIN';
+}
 
+function getEmail(user){
+    return user.firstName.toLowerCase() + '.' + user.lastName.toLowerCase() + '@codeimmersives.com';
+}
 
+function getPlaylistLength(playlist){
+    return playlist.songs.length;
+}
+
+function getHardestHomework(arr){
+    if(arr.length < 1){
+        return '';
+    }
+    let min = 100;
+    for(let i of arr){
+        min = Math.min(min, i.averageScore);
+    }
+    for(let i of arr){
+        if(i.averageScore === min){
+            return i.name;
+        }
+    }
+}
+
+function createPhonebook(arrName, arrNum){
+    let phoneBook ={};
+    if(arrName.length < 1){
+        return phoneBook;
+    }
+    for(let i = 0; i < arrName.length; i++){
+        phoneBook[arrName[i]] = arrNum[i];
+    }
+    return phoneBook;
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
